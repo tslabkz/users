66a9dff640d7911cfcc131655bd1744a865ddbcb
Abdurakhman <a.taskibayev@mediana.kz>
2024-05-12 02:48:32 +0500

    login page works perfectly

---
+++ js/project.js
@@ -0,0 +1,8 @@
+var projectClass = function() {
+    return {
+        api_url: "https://script.google.com/macros/s/AKfycbyfErWgLInHWVu0WfO4hvqLBLN564a98uz-aiVgvl6wk61pntoVuQyzaorsUws-Vp_n/exec",
+        default_service_id: "mektep",
+    }
+}
+
+const project = new projectClass();
