224f2ca77d93cec248681f30bbe25c1bf8000e48
Abdurakhman <a.taskibayev@mediana.kz>
2024-05-12 14:40:36 +0500

    list of actions

--- js/project.js
+++ js/project.js
@@ -1,7 +1,23 @@
 var projectClass = function() {
+    var api_url = "https://script.google.com/macros/s/AKfycbw1wAThtRLXq5606G7nbhJk_bpFdF7eHfGDNOL4LpxFdwsmsw5mxykXQBR21ZTD7E00/exec";
+    var default_service_id = "mektep";
+
+    //  project.api_url + '?' + $.param(data);
+
+    function buildUrl(action, data) {
+        const url = new URL(api_url);
+        url.searchParams.set('action', action);
+        url.searchParams.set('service_id', default_service_id);
+        Object.keys(data).forEach(function(key){    
+            url.searchParams.set(key, data[key]);
+        });
+        return url.toString();
+    }
+
     return {
-        api_url: "https://script.google.com/macros/s/AKfycbyfErWgLInHWVu0WfO4hvqLBLN564a98uz-aiVgvl6wk61pntoVuQyzaorsUws-Vp_n/exec",
-        default_service_id: "mektep",
+        api_url,
+        default_service_id,
+        buildUrl,
     }
 }
 
