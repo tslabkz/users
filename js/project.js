22e495a9a0e9f27e953d5cf9b2eb319a0a8b4e19
Abdyrakhman Taskibayev <taskibaev.ab@gmail.com>
2024-05-23 13:18:56 +0500

    выводим список для подтверждения

--- js/project.js
+++ js/project.js
@@ -1,6 +1,8 @@
 var projectClass = function() {
-    var api_url = "https://script.google.com/macros/s/AKfycbw1wAThtRLXq5606G7nbhJk_bpFdF7eHfGDNOL4LpxFdwsmsw5mxykXQBR21ZTD7E00/exec";
-    var default_service_id = "mektep";
+    // var api_url = "https://script.google.com/macros/s/AKfycbw1wAThtRLXq5606G7nbhJk_bpFdF7eHfGDNOL4LpxFdwsmsw5mxykXQBR21ZTD7E00/exec";
+    var api_url = "https://script.google.com/macros/s/AKfycbySmaITEMroIEzoE6wwKEW9mchZx9tZ8e3ZLnse0HmL8Hf7ydE5-SwS1s2F5gI2Dca2/exec";
+ // var api_url = "https://script.google.com/macros/s/AKfycbySmaITEMroIEzoE6wwKEW9mchZx9tZ8e3ZLnse0HmL8Hf7ydE5-SwS1s2F5gI2Dca2/exec";
+    var default_service_id = "mediana";
 
     //  project.api_url + '?' + $.param(data);
 
